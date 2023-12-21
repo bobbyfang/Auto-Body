a7692b434de9e403d0ce3053b62b9c5fe7ba208b
Bobby Fang <bobbyfang@users.noreply.github.com>
2023-12-21 16:26:12 +0200

    Customer model commit

--- backend/customers/admin.py
+++ backend/customers/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Customer, ContactPerson
 
 # Register your models here.
+
+
+class ContactPersonInline(admin.StackedInline):
+    model = ContactPerson
+    classes = ["collapse"]
+    extra = 0
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    inlines = [
+        ContactPersonInline,
+    ]
+    # list_display = ('customer_')
+
+
+# admin.site.register(ContactPerson)
+admin.site.register(Customer, CustomerAdmin)
--- backend/customers/models.py
+++ backend/customers/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+# from django.contrib.auth.models import User
+
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    customer_number = models.CharField(primary_key=True, max_length=128)
+    name = models.CharField(max_length=128)
+    telephone_number = PhoneNumberField(default="")
+    fax_number = PhoneNumberField(default="", blank=True)
+    mobile_number = PhoneNumberField(default="", blank=True)
+    email = models.EmailField(default="", blank=True)
+    physical_address = models.TextField(default="", blank=True)
+    billing_address = models.TextField(default="", blank=True)
+
+    credit_limit = models.DecimalField(max_digits=16,
+                                       default=1000.00,
+                                       decimal_places=2)
+
+    vat_number = models.CharField(max_length=10,
+                                  default="",
+                                  name="VAT Number",
+                                  blank=True)
+    company_key_number = models.CharField(max_length=32,
+                                          default="",
+                                          name="CK Number",
+                                          blank=True)
+
+    shipping_instructions = models.TextField(default="", blank=True)
+    memo = models.TextField(default="", blank=True)
+
+    suspend = models.BooleanField(default=False)
+    suspend_date = models.DateField(default=None, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class ContactPerson(models.Model):
+    contact_name = models.CharField(max_length=128)
+    role = models.CharField(max_length=128, blank=True, default="")
+    phone_number = PhoneNumberField()
+    email = models.EmailField(blank=True, default="")
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.contact_name)
