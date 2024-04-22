65aa8da947581896c673790a38e8b108cea8dbaf
Bobby Fang <bobbyfang@users.noreply.github.com>
2024-04-22 09:40:36 +0200

    Backend ---------- -Implemented custom reference number field for models based tally of current date -Changed ReferenceModel abstract class to use updated reference number field

--- backend/common/abstract/models.py
+++ backend/common/abstract/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from common.abstract.fields import ReferenceNumberField
+
 
 class ReferenceModel(models.Model):
-    reference_number = models.CharField(
-        primary_key=True, max_length=9, default="", editable=False
-    )
+    reference_number = ReferenceNumberField(primary_key=True)
     created = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.PROTECT)
 
